75ed1fb5a07eb9ec8b4df38db0af49d735061c77
Aty <aty99@yahoo.com>
2020-03-25 13:46:50 +0200

    Initial Commit

---
+++ SwipeImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeImage
+//
+//  Created by Mohamed Abd el Aty on 3/25/20.
+//  Copyright © 2020 Aty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
